2379311ec3ab824c5f499f2b5a53b1e559c69244
Pere Bohigas <pboh@num42.de>
2021-12-07 23:37:52 +0100

    Add The Treachery of Whales (day 7) part two solution

--- swift/Day_7_The_Treachery_of_Whales.swift
+++ swift/Day_7_The_Treachery_of_Whales.swift
@@ -62,10 +62,56 @@ let positionWeights: [(position: Int, weight: Int)] = initialRange.map { positio
     return (position, weight)
   }
 
-let optimalPosition = positionWeights
-  .min(by: { x, y in
-    x.weight < y.weight
-  })!
+let optimalPosition = positionWeights.min { $0.weight < $1.weight }!
 
 print("Fuel consumption for optimal align (part one): \(optimalPosition.weight)")
+
+// --- Part Two ---
+//
+// The crabs don't seem interested in your proposed solution. Perhaps you misunderstand crab engineering?
+//
+// As it turns out, crab submarine engines don't burn fuel at a constant rate. Instead, each change of 1 step in horizontal position costs 1 more unit of fuel than the last: the first step costs 1, the second step costs 2, the third step costs 3, and so on.
+//
+// As each crab moves, moving further becomes more expensive. This changes the best horizontal position to align them all on; in the example above, this becomes 5:
+//
+//   - Move from 16 to 5: 66 fuel
+//   - Move from 1 to 5: 10 fuel
+//   - Move from 2 to 5: 6 fuel
+//   - Move from 0 to 5: 15 fuel
+//   - Move from 4 to 5: 1 fuel
+//   - Move from 2 to 5: 6 fuel
+//   - Move from 7 to 5: 3 fuel
+//   - Move from 1 to 5: 10 fuel
+//   - Move from 2 to 5: 6 fuel
+//   - Move from 14 to 5: 45 fuel
+//
+// This costs a total of 168 fuel. This is the new cheapest possible outcome; the old alignment position (2) now costs 206 fuel instead.
+//
+// Determine the horizontal position that the crabs can align to using the least fuel possible so they can make you an escape route! How much fuel must they spend to align to that position?
+//
+// from: https://adventofcode.com/2021/day/7#part2
+
+// Solution Part Two:
+
+let positionsFrequencies2 = Dictionary(initialPositions.map { ($0, 1) }, uniquingKeysWith: +)
+
+let positionWeights2: [(position: Int, weight: Int)] = initialRange.map { position in
+    let weight = positionsFrequencies2
+      .map { frequency -> Int in
+        let distance = abs(position - frequency.key)
+
+        // Formula for triangular numbers
+        // from: https://en.wikipedia.org/wiki/Triangular_number
+        let consumption = distance * (distance + 1) / 2
+
+        return consumption * frequency.value
+      }
+      .reduce(0,+)
+
+    return (position, weight)
+  }
+
+let optimalPosition2 = positionWeights2.min { $0.weight < $1.weight }!
+
+print("Fuel consumption for optimal align (part two): \(optimalPosition2.weight)")
 
