4894a20a0bff3098812abc609c6fab781168b309
Pere Bohigas <pboh@num42.de>
2021-12-10 22:58:17 +0100

    Add Syntax Scoring (day 10) part two solution for swift

--- swift/Day_10_Syntax_Scoring.swift
+++ swift/Day_10_Syntax_Scoring.swift
@@ -72,6 +72,23 @@ let navigationSubsystem = input.split(separator: "\n")
 
 // from: https://adventofcode.com/2021/day/9/input
 
+// Test data:
+
+let testData = [
+    "[({(<(())[]>[[{[]{<()<>>",
+    "[(()[<>])]({[<{<<[]>>(",
+    "{([(<{}[<>[]}>{[]{[(<()>",
+    "(((({<>}<{<{<>}{[]{[]{}",
+    "[[<[([]))<([[{}[[()]]]",
+    "[{[{({}]{}}([{[{{{}}([]",
+    "{<[[]]>}<{[{[{[]{()[[[]",
+    "[<(<(<(<{}))><([]([]()",
+    "<{([([[(<>()){}]>(<<{{",
+    "<{([{{}}[<[[[<>{}]]]>[]"
+  ].map {
+    $0.compactMap(Character.init)
+  }
+
 // Solution Part One:
 
 typealias CharacterPair = (opening: Character, closing: Character)
@@ -85,12 +102,12 @@ let corruptedLineCharacters = navigationSubsystem.compactMap { line -> Character
     if chunkCharacters.map(\.opening).contains(character) {
       charactersStack.append(character)
     } else if chunkCharacters.map(\.closing).contains(character) {
-      let chunkStart = chunkCharacters.filter { $0.closing == character }.first!.opening
+      let chunkStart: Character = chunkCharacters.filter { $0.closing == character }.first!.opening
       if chunkStart == charactersStack.last! {
         _ = charactersStack.popLast()
       } else {
-        let expectedClosing = chunkCharacters.filter { $0.opening == charactersStack.last! }.first!.closing
-        print("\(line) - Expected \(expectedClosing), but found \(character) instead.")
+        let expectedClosing: Character = chunkCharacters.filter { $0.opening == charactersStack.last! }.first!.closing
+        print(line.compactMap(String.init).reduce("", +) + " - Expected \(expectedClosing), but found \(character) instead.")
         corruptedCharacter = character
         break
       }
@@ -107,4 +124,94 @@ let totalSyntaxErrorScore = corruptedLineCharacters.reduce(0, { accumulated, cha
 })
 
 print("Answer (part one) - Total syntax error score: \(totalSyntaxErrorScore)")
+
+// --- Part Two ---
+//
+// Now, discard the corrupted lines. The remaining lines are incomplete.
+//
+// Incomplete lines don't have any incorrect characters - instead, they're missing some closing characters at the end of the line. To repair the navigation subsystem, you just need to figure out the sequence of closing characters that complete all open chunks in the line.
+//
+// You can only use closing characters (), ], }, or >), and you must add them in the correct order so that only legal pairs are formed and all chunks end up closed.
+//
+// In the example above, there are five incomplete lines:
+//
+//   - [({(<(())[]>[[{[]{<()<>> - Complete by adding }}]])})].
+//   - [(()[<>])]({[<{<<[]>>( - Complete by adding )}>]}).
+//   - (((({<>}<{<{<>}{[]{[]{} - Complete by adding }}>}>)))).
+//   - {<[[]]>}<{[{[{[]{()[[[] - Complete by adding ]]}}]}]}>.
+//   - <{([{{}}[<[[[<>{}]]]>[]] - Complete by adding ])}>.
+//
+// Did you know that autocomplete tools also have contests? It's true! The score is determined by considering the completion string character-by-character. Start with a total score of 0. Then, for each character, multiply the total score by 5 and then increase the total score by the point value given for the character in the following table:
+//
+//   - ): 1 point.
+//   - ]: 2 points.
+//   - }: 3 points.
+//   - >: 4 points.
+//
+// So, the last completion string above - ])}> - would be scored as follows:
+//
+//   - Start with a total score of 0.
+//   - Multiply the total score by 5 to get 0, then add the value of ] (2) to get a new total score of 2.
+//   - Multiply the total score by 5 to get 10, then add the value of ) (1) to get a new total score of 11.
+//   - Multiply the total score by 5 to get 55, then add the value of } (3) to get a new total score of 58.
+//   - Multiply the total score by 5 to get 290, then add the value of > (4) to get a new total score of 294.
+//
+// The five lines' completion strings have total scores as follows:
+//
+//   - }}]])})] - 288957 total points.
+//   - )}>]}) - 5566 total points.
+//   - }}>}>)))) - 1480781 total points.
+//   - ]]}}]}]}> - 995444 total points.
+//   - ])}> - 294 total points.
+//
+// Autocomplete tools are an odd bunch: the winner is found by sorting all of the scores and then taking the middle score. (There will always be an odd number of scores to consider.) In this example, the middle score is 288957 because there are the same number of scores smaller and larger than it.
+//
+// Find the completion string for each incomplete line, score the completion strings, and sort the scores. What is the middle score?
+//
+// from: https://adventofcode.com/2021/day/10#part2
+
+// Solution Part Two:
+
+let lineClosingCharacters = navigationSubsystem.compactMap { line -> [Character]? in
+  var charactersStack = [Character]()
+
+  for character in line {
+    if chunkCharacters.map(\.opening).contains(character) {
+      charactersStack.append(character)
+    } else if chunkCharacters.map(\.closing).contains(character) {
+      let chunkStart = chunkCharacters.filter { $0.closing == character }.first!.opening
+      if chunkStart == charactersStack.last! {
+        _ = charactersStack.popLast()
+      } else {
+        // Removing corrupted lines
+        return nil
+      }
+    }
+  }
+
+  let closingCharacters = charactersStack.reversed().map { character in
+    chunkCharacters.filter { $0.opening == character }.first!.closing
+  }
+
+  print(line.compactMap(String.init).reduce("", +) + " - Completed by adding " + closingCharacters.compactMap(String.init).reduce("", +))
+
+  return closingCharacters
+}
+
+let closingCharacterPoints: [Character:Int] = [")": 1, "]":2, "}":3, ">":4]
+
+let lineScores = lineClosingCharacters.map { lineClosingCharacters -> Int in
+  let lineScore = lineClosingCharacters.reduce(0, { acumulated, character in
+    (acumulated * 5) + closingCharacterPoints[character]!
+  })
+
+  print(lineClosingCharacters.compactMap(String.init).reduce("", +) + " - \(lineScore) total points.")
+
+  return lineScore
+}.sorted()
+
+
+let middleScore = lineScores[(lineScores.count - 1) / 2]
+
+print("Answer (part two) - Middle score: \(middleScore)")
 
