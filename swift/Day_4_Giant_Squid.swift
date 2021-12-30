87f9450ff7ea793bac248f67e7fd1baa9d68b464
Pere Bohigas <pboh@num42.de>
2021-12-30 11:32:11 +0100

    Add test data for Giant Squid (day 4) on swift

--- swift/Day_4_Giant_Squid.swift
+++ swift/Day_4_Giant_Squid.swift
@@ -84,6 +84,41 @@ let boards: [[[Int]]] = {
 
 // from: https://adventofcode.com/2021/day/4/input
 
+let testData = """
+  7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+
+  22 13 17 11  0
+  8  2 23  4 24
+  21  9 14 16  7
+  6 10  3 18  5
+  1 12 20 15 19
+
+  3 15  0  2 22
+  9 18 13 17  5
+  19  8  7 25 23
+  20 11 10 24  4
+  14 21 16 12  6
+
+  14 21 17 24  4
+  10 16 15  9 19
+  18  8 23 26 20
+  22 11 13  6  5
+  2  0 12  3  7
+  """
+
+let testNumbersToDraw: [Int] = testData.split(separator: "\n")[0].split(separator: ",").compactMap { Int($0) }
+
+let testBoards: [[[Int]]] = {
+    let boardData = testData.split(separator: "\n").dropFirst().map(String.init)
+
+    let rows = stride(from: 0, to: boardData.count, by: boardSize.column).map {
+      Array(boardData[$0 ..< Swift.min($0 + boardSize.column, boardData.count)])
+    }
+
+    let boards = rows.map { $0.map { $0.split(separator: " ").compactMap { Int($0) } } }
+    return boards
+  }()
+
 // Solution Part One:
 
 var winningNumber: Int? = nil
